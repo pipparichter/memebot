370fc968f75b65d343062db8d51a83e424a13290
Philippa Richter <philippa.a.richter@gmail.com>
2019-06-17 09:07:06 -0400

    adding an app function

--- memebot.py
+++ memebot.py
@@ -16,25 +16,26 @@ botID = global_vars.botID
 
 requestParams = {'token': token, 'limit':1}
 
-while True:
-    rqResponse = requests.get('https://api.groupme.com/v3/groups/' + groupID +'/messages', params = requestParams)
+def app():
+    while True:
+        rqResponse = requests.get('https://api.groupme.com/v3/groups/' + groupID +'/messages', params = requestParams)
     
-    # Pings the gm-membot Heroku app so it doesn't idle.
-    requests.get('http://gm-memebot.herokuapp.com') 
+        # Pings the gm-membot Heroku app so it doesn't idle.
+        requests.get('http://gm-memebot.herokuapp.com') 
     
-    if rqResponse.status_code == 200:
-        gotten = rqResponse.json()['response']['messages']
+        if rqResponse.status_code == 200:
+            gotten = rqResponse.json()['response']['messages']
         
-        for message in gotten:
-            messageText = message['text'].lower()
+            for message in gotten:
+                messageText = message['text'].lower()
                       
-            if (messageText in bot_reply.staticTriggers) or (messageText in bot_reply.dynamicTriggers):
-                bot_reply.botReply(message)
-                requestParams['since_id'] = message['id']
+                if (messageText in bot_reply.staticTriggers) or (messageText in bot_reply.dynamicTriggers):
+                    bot_reply.botReply(message)
+                    requestParams['since_id'] = message['id']
         
-    else:
-        raise Exception('GroupMe\'s API fucked up.')
-        break
+        else:
+            raise Exception('GroupMe\'s API fucked up.')
+            break
     
     
-    time.sleep(5)
+        time.sleep(5)
