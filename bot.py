82051a57880d54d1341961b6ed1abff6159203ae
Philippa Richter <philippa.a.richter@gmail.com>
2019-06-23 07:38:27 -0400

    adding WSGI app

--- bot.py
+++ bot.py
@@ -1,4 +1,10 @@
+from wsgiref.simple_server import make_server
+import cgi
+
+import requests
+import time
 import sys
+
 sys.path.append('./memebot/features')
 sys.path.append('./memebot/dictionaries')
 sys.path.append('./memebot')
@@ -6,39 +12,43 @@ sys.path.append('./memebot')
 import global_vars
 import bot_reply
 
-import requests
-import time
-
 
 token = global_vars.token
 groupID = global_vars.groupID
 botID = global_vars.botID
 
-requestParams = {'token': token, 'limit':1}
+requestParams = {'token':token, 'limit':1}
 
-def app():
-    while True:
-        requestResponse = requests.get('https://api.groupme.com/v3/groups/' + groupID +'/messages', params = requestParams)
-        
-        # Pings the gm-membot Heroku app so it doesn't idle.
-        # herokuResponse = requests.get('http://gm-memebot.herokuapp.com' timeout = 0.5) 
-        # if herokuResponse.status_code == 200:
-            # bot_reply.sendMessage('Heroku is working')
 
-        if requestResponse.status_code == 200:
-            gotten = requestResponse.json()['response']['messages']
-            for message in gotten:
-                messageText = message['text'].lower()
-                      
-                if (messageText in bot_reply.staticTriggers) or (messageText in bot_reply.dynamicTriggers):
-                    bot_reply.botReply(message)
-                    requestParams['since_id'] = message['id']
-        
-        else:
-            error = requestResponse.status_code
-            raise Exception('Something went wrong. GroupMe API gave error %s' % error)
-            break
+def app(environ, startResponse):
     
-        time.sleep(5)
+    try:
+        requestBodySize = int(environ.get('CONTENT_LENGTH', 0))
+    except ValueError:
+        requestBodySize = 0
+        
+    requestBody = environ['wsgi.input'].read(requestBodySize)
+    print('something') 
 
-app = app()
+    # bodyDict = cgi.parse_qs(requestBody)
+    # print(bodyDict)
+
+    responseBody = bytes('successful', 'utf-8') 
+       
+    status = '200 OK'
+    responseHeaders = [('Content-Type', 'text/plain'), ('Content-Length', str(len(responseBody)))]
+
+    startResponse(status, responseHeaders)
+
+    return [responseBody]
+
+
+# port = os.environ['PORT']
+# print(port)
+
+# server = make_server('gm-memebot.herokuapp.com', port, website)
+server = make_server('localhost', 810, app)
+
+# print(os.environ['PORT'])
+
+server.serve_forever()
