41312da0a37d96063492887d50fdd90e2ec6d906
Philippa Richter <philippa.a.richter@gmail.com>
2019-06-24 20:42:00 -0400

    working!

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ def app(environ, startResponse):
         requestBodySize = 0
         
     requestBody = environ['wsgi.input'].read(requestBodySize)
-    bot_reply.sendMessage(requestBody)     
+    bot_reply.sendMessage(str(requestBody))     
 
     bodyDict = cgi.parse_qs(requestBody)
     # print(bodyDict)
